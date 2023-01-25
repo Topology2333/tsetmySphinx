2123b3acb9bf92be3bf5801fe3aa423dee2b51e0
“Topology2333” <stephendedalus17@gmail.com>
2023-01-25 18:41:48 +0800

    cg py

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,10 @@ author = 'Std'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+	'recommonmark',
+	'sphinx_markdown_tables'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
